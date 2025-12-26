e63dfd473f7d80f0580c66b6dcc39a548b731e64
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2025-12-26 14:28:51 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/org/umc/valuedi/ValuediApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umc.valuedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValuediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
