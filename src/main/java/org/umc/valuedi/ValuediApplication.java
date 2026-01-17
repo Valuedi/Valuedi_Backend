9a7bb667ab16921ad1d299958942b531e5e7f56e
kkeunii <greenlee55@naver.com>
2026-01-17 21:23:32 +0900

    fix:충돌 해결 ㅠㅠ

--- src/main/java/org/umc/valuedi/ValuediApplication.java
+++ src/main/java/org/umc/valuedi/ValuediApplication.java
@@ -2,10 +2,12 @@ package org.umc.valuedi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 public class ValuediApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ValuediApplication.class, args);
