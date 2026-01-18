f81483d746caa44dd1ae8a0c1c83cb0f29f76941
JeongGyul <jjascd@naver.com>
2026-01-19 00:10:44 +0900

    feat: 이메일 인증번호 발송/검증 API 구현

--- src/main/java/org/umc/valuedi/domain/auth/exception/code/AuthSuccessCode.java
+++ src/main/java/org/umc/valuedi/domain/auth/exception/code/AuthSuccessCode.java
@@ -11,7 +11,9 @@ public enum AuthSuccessCode implements BaseSuccessCode {
 
     KAKAO_AUTH_URL_SUCCESS(HttpStatus.OK, "AUTH200_1", "카카오 로그인 URL이 성공적으로 생성되었습니다."),
     LOGIN_OK(HttpStatus.OK, "AUTH200_2", "로그인에 성공했습니다."),
-    USERNAME_AVAILABLE(HttpStatus.OK, "AUTH200_3", "사용 가능한 아이디입니다.")
+    USERNAME_AVAILABLE(HttpStatus.OK, "AUTH200_3", "사용 가능한 아이디입니다."),
+    EMAIL_SEND_SUCCESS(HttpStatus.OK, "AUTH200_4", "인증번호가 발송되었습니다."),
+    EMAIL_VERIFY_SUCCESS(HttpStatus.OK, "AUTH200_5", "이메일 인증에 성공했습니다.")
     ;
 
     private final HttpStatus status;
--- src/main/java/org/umc/valuedi/domain/auth/service/command/AuthCommandService.java
+++ src/main/java/org/umc/valuedi/domain/auth/service/command/AuthCommandService.java
@@ -4,10 +4,16 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.umc.valuedi.domain.auth.converter.AuthConverter;
 import org.umc.valuedi.domain.auth.dto.kakao.KakaoResDTO;
 import org.umc.valuedi.domain.auth.dto.res.AuthResDTO;
+import org.umc.valuedi.domain.auth.exception.AuthException;
+import org.umc.valuedi.domain.auth.exception.code.AuthErrorCode;
 import org.umc.valuedi.domain.auth.service.external.KakaoService;
 import org.umc.valuedi.domain.member.entity.Member;
 import org.umc.valuedi.domain.member.entity.MemberAuthProvider;
@@ -22,15 +28,22 @@ import org.umc.valuedi.global.apiPayload.exception.GeneralException;
 import org.umc.valuedi.global.security.jwt.JwtUtil;
 import org.umc.valuedi.global.security.principal.CustomUserDetails;
 
+import java.security.SecureRandom;
+import java.time.Duration;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class AuthCommandService {
     private final JwtUtil jwtUtil;
     private final KakaoService kakaoService;
+    private final JavaMailSender mailSender;
+    private final StringRedisTemplate redisTemplate;
     private final MemberRepository memberRepository;
     private final MemberAuthProviderRepository memberAuthProviderRepository;
+    private static final SecureRandom sr = new SecureRandom();
 
+    // 카카오 로그인
     public AuthResDTO.LoginResultDTO loginKakao(String code) {
         KakaoResDTO.UserInfoDTO userInfo = kakaoService.getKakaoUserInfo(code);
         String providerUserId = String.valueOf(userInfo.getId());
@@ -53,6 +66,7 @@ public class AuthCommandService {
         return AuthConverter.toLoginResultDTO(member, accessToken, refreshToken);
     }
 
+    // 카카오로 회원가입
     private Member registerKakao(KakaoResDTO.UserInfoDTO userInfo) {
         try {
             Member newMember = AuthConverter.toMember(userInfo);
@@ -67,5 +81,40 @@ public class AuthCommandService {
         } catch(DataIntegrityViolationException e) {
             throw new GeneralException(GeneralErrorCode.INVALID_DATA_REQUEST);
         }
+    }
+
+    // 이메일 인증번호 발송
+    public void sendCode(String email) {
+        String code = String.valueOf(sr.nextInt(900_000) + 100_000);
+        String redisKey = "AUTH_CODE" + email;
+        redisTemplate.opsForValue().set(redisKey, code, Duration.ofMinutes(3));
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email);
+            message.setSubject("[Valuedi] 회원가입 인증번호입니다.");
+            message.setText("인증번호는 [" + code + "] 입니다. 3분 이내에 입력해 주세요.");
+            mailSender.send(message);
+        } catch (MailException e) {
+            redisTemplate.delete(redisKey);
+            throw new AuthException(AuthErrorCode.MAIL_SEND_ERROR);
+        }
+    }
+
+    // 이메일 인증번호 검증
+    public void verifyCode(String email, String code) {
+        String redisKey = "AUTH_CODE:" + email;
+        String savedCode = redisTemplate.opsForValue().get(redisKey);
+
+        if(savedCode == null) {
+            throw new AuthException(AuthErrorCode.EMAIL_CODE_NOT_FOUND);
+        }
+
+        if(!savedCode.equals(code)) {
+            throw new AuthException(AuthErrorCode.EMAIL_CODE_MISMATCH);
+        }
+
+        redisTemplate.opsForValue().set("EMAIL_VERIFIED:" + email, "true", Duration.ofMinutes(5));
+        redisTemplate.delete(redisKey);
     }
 }
