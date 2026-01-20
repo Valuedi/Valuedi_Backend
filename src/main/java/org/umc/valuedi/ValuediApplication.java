850da2774251c77abcce8e53df1ce116038a1c1e
hodoon <ehgns79513@gmial.com>
2026-01-20 11:22:37 +0900

    feat : Trophy 달성 여부 계산 스케줄링 배치작업 추가

--- src/main/java/org/umc/valuedi/ValuediApplication.java
+++ src/main/java/org/umc/valuedi/ValuediApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
+@EnableScheduling
 @ConfigurationPropertiesScan
 public class ValuediApplication {
 	public static void main(String[] args) {
