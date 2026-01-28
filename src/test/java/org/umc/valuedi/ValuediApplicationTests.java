a6f5387b2b5fdf8ca757228b1f75eca13398a591
hodoon <ehgns79513@gmial.com>
2026-01-28 13:58:51 +0900

    feat : 테스트케이스 DB mySQL로 변경

--- src/test/java/org/umc/valuedi/ValuediApplicationTests.java
+++ src/test/java/org/umc/valuedi/ValuediApplicationTests.java
@@ -10,8 +10,21 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 class ValuediApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Container
+	static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+			.withDatabaseName("valuedi_test")
+			.withUsername("mock")
+			.withPassword("mock");
+
+	@DynamicPropertySource
+	static void overrideProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mysql::getJdbcUrl);
+		registry.add("spring.datasource.username", mysql::getUsername);
+		registry.add("spring.datasource.password", mysql::getPassword);
+		registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
 	}
+
+	@Test
+	void contextLoads() {}
 
 }
