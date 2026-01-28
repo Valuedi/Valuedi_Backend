24aef21125e1dfc52de948b0fa389741bd277218
hodoon <ehgns79513@gmial.com>
2026-01-28 14:16:39 +0900

    fix : 테스트케이스 DB 문제 해결

--- src/test/java/org/umc/valuedi/ValuediApplicationTests.java
+++ src/test/java/org/umc/valuedi/ValuediApplicationTests.java
@@ -2,27 +2,11 @@ package org.umc.valuedi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootTest
 @EnableScheduling
 class ValuediApplicationTests {
-
-	@Container
-	static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-			.withDatabaseName("valuedi_test")
-			.withUsername("mock")
-			.withPassword("mock");
-
-	@DynamicPropertySource
-	static void overrideProps(DynamicPropertyRegistry registry) {
-		registry.add("spring.datasource.url", mysql::getJdbcUrl);
-		registry.add("spring.datasource.username", mysql::getUsername);
-		registry.add("spring.datasource.password", mysql::getPassword);
-		registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
-	}
 
 	@Test
 	void contextLoads() {}
