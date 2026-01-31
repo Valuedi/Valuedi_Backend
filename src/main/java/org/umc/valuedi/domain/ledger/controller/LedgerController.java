e293d06c47e265e1580164ababa2329ee636d7e5
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2026-01-31 22:02:45 +0900

    refactor: @CurrentMember로 memberId 주입 방식 변경

--- src/main/java/org/umc/valuedi/domain/ledger/controller/LedgerController.java
+++ src/main/java/org/umc/valuedi/domain/ledger/controller/LedgerController.java
@@ -10,6 +10,7 @@ import org.umc.valuedi.domain.ledger.exception.code.LedgerSuccessCode;
 import org.umc.valuedi.domain.ledger.service.command.LedgerSyncService;
 import org.umc.valuedi.domain.ledger.service.query.LedgerQueryService;
 import org.umc.valuedi.global.apiPayload.ApiResponse;
+import org.umc.valuedi.global.security.annotation.CurrentMember;
 import org.umc.valuedi.global.security.principal.CustomUserDetails;
 
 import java.time.LocalDate;
@@ -24,50 +25,42 @@ public class LedgerController implements LedgerControllerDocs {
     private final LedgerSyncService ledgerSyncService;
 
     @Override
-    public ApiResponse<LedgerListResponse> getTransactions(@AuthenticationPrincipal CustomUserDetails userDetails, YearMonth yearMonth, LocalDate date, int page, int size, LedgerSortType sort) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<LedgerListResponse> getTransactions(@CurrentMember Long memberId, YearMonth yearMonth, LocalDate date, int page, int size, LedgerSortType sort) {
         return ApiResponse.onSuccess(LedgerSuccessCode.LEDGER_LIST_FETCHED, ledgerQueryService.getTransactions(memberId, yearMonth, date, page, size, sort));
     }
 
     @Override
-    public ApiResponse<LedgerSummaryResponse> getMonthlySummary(@AuthenticationPrincipal CustomUserDetails userDetails, YearMonth yearMonth) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<LedgerSummaryResponse> getMonthlySummary(@CurrentMember Long memberId, YearMonth yearMonth) {
         return ApiResponse.onSuccess(LedgerSuccessCode.LEDGER_SUMMARY_FETCHED, ledgerQueryService.getMonthlySummary(memberId, yearMonth));
     }
 
     @Override
-    public ApiResponse<List<CategoryStatResponse>> getCategoryStats(@AuthenticationPrincipal CustomUserDetails userDetails, YearMonth yearMonth) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<List<CategoryStatResponse>> getCategoryStats(@CurrentMember Long memberId, YearMonth yearMonth) {
         return ApiResponse.onSuccess(LedgerSuccessCode.CATEGORY_STATS_FETCHED, ledgerQueryService.getCategoryStats(memberId, yearMonth));
     }
 
     @Override
-    public ApiResponse<List<DailyStatResponse>> getDailyStats(@AuthenticationPrincipal CustomUserDetails userDetails, YearMonth yearMonth) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<List<DailyStatResponse>> getDailyStats(@CurrentMember Long memberId, YearMonth yearMonth) {
         return ApiResponse.onSuccess(LedgerSuccessCode.DAILY_STATS_FETCHED, ledgerQueryService.getDailyStats(memberId, yearMonth));
     }
 
     @Override
-    public ApiResponse<List<TrendResponse>> getTrend(@AuthenticationPrincipal CustomUserDetails userDetails, YearMonth fromYearMonth, YearMonth toYearMonth) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<List<TrendResponse>> getTrend(@CurrentMember Long memberId, YearMonth fromYearMonth, YearMonth toYearMonth) {
         return ApiResponse.onSuccess(LedgerSuccessCode.TREND_FETCHED, ledgerQueryService.getTrend(memberId, fromYearMonth, toYearMonth));
     }
 
     @Override
-    public ApiResponse<List<TopCategoryResponse>> getTopCategories(@AuthenticationPrincipal CustomUserDetails userDetails, YearMonth yearMonth, int limit) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<List<TopCategoryResponse>> getTopCategories(@CurrentMember Long memberId, YearMonth yearMonth, int limit) {
         return ApiResponse.onSuccess(LedgerSuccessCode.TOP_CATEGORY_FETCHED, ledgerQueryService.getTopCategories(memberId, yearMonth, limit));
     }
 
     @Override
-    public ApiResponse<PeerCompareResponse> getPeerComparison(@AuthenticationPrincipal CustomUserDetails userDetails, YearMonth yearMonth) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<PeerCompareResponse> getPeerComparison(@CurrentMember Long memberId, YearMonth yearMonth) {
         return ApiResponse.onSuccess(LedgerSuccessCode.PEER_COMPARE_FETCHED, ledgerQueryService.getPeerComparison(memberId, yearMonth));
     }
 
     @Override
-    public ApiResponse<String> syncTransactions(@AuthenticationPrincipal CustomUserDetails userDetails, LedgerSyncRequest request) {
-        Long memberId = Long.valueOf(userDetails.getUsername());
+    public ApiResponse<String> syncTransactions(@CurrentMember Long memberId, LedgerSyncRequest request) {
         ledgerSyncService.syncTransactions(memberId, request);
         return ApiResponse.onSuccess(LedgerSuccessCode.LEDGER_SYNC_SUCCESS, "거래내역 동기화가 완료되었습니다.");
     }
