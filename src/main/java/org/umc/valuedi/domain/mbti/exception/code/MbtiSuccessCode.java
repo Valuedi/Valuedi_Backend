49215095108d7853cc504d827355fcae280f30d1
kkeunii <greenlee55@naver.com>
2026-02-01 16:25:28 +0900

    refactor: 에러 및 성공 코드 수정

--- src/main/java/org/umc/valuedi/domain/mbti/exception/code/MbtiSuccessCode.java
+++ src/main/java/org/umc/valuedi/domain/mbti/exception/code/MbtiSuccessCode.java
@@ -9,10 +9,10 @@ import org.umc.valuedi.global.apiPayload.code.BaseSuccessCode;
 @RequiredArgsConstructor
 public enum MbtiSuccessCode implements BaseSuccessCode {
 
-    MBTI_TEST_SUBMITTED(HttpStatus.CREATED, "201_1", "금융 MBTI 테스트 제출에 성공했습니다."),
-    MBTI_QUESTIONS_FETCHED(HttpStatus.OK, "GOAL200_1", "금융 MBTI 문항 조회에 성공했습니다."),
-    MBTI_RESULT_FETCHED(HttpStatus.OK, "GOAL200_2", "금융 MBTI 결과 조회에 성공했습니다."),
-    MBTI_TYPE_LIST_FETCHED(HttpStatus.OK, "GOAL200_3", "금융 MBTI 유형 목록 조회에 성공했습니다.");
+    MBTI_TEST_SUBMITTED(HttpStatus.CREATED, "MBTI_200_1", "금융 MBTI 테스트 제출에 성공했습니다."),
+    MBTI_QUESTIONS_FETCHED(HttpStatus.OK, "MBTI_201_1", "금융 MBTI 문항 조회에 성공했습니다."),
+    MBTI_RESULT_FETCHED(HttpStatus.OK, "MBTI_201_2", "금융 MBTI 결과 조회에 성공했습니다."),
+    MBTI_TYPE_LIST_FETCHED(HttpStatus.OK, "MBTI_201_3", "금융 MBTI 유형 목록 조회에 성공했습니다.");
 
     private final HttpStatus status;
     private final String code;
