cc53b54f362d724a6f77d1b09030951aeedadcc1
kkeunii <greenlee55@naver.com>
2026-02-19 14:08:31 +0900

    refactor: pr 코멘트 반영

--- src/main/java/org/umc/valuedi/domain/goal/service/GoalStatusChangeService.java
+++ src/main/java/org/umc/valuedi/domain/goal/service/GoalStatusChangeService.java
@@ -1,6 +1,6 @@
 package org.umc.valuedi.domain.goal.service;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
